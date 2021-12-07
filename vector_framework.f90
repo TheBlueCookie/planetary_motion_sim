1175e0deeb13a903330110a5be1fc9c9010e319b
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-07 18:51:18 +0200

    restructured vector type in own module file, added operator overloading

---
+++ vector_framework.f90
@@ -0,0 +1,53 @@
+module vectors
+    implicit none
+    integer, parameter :: rk = selected_real_kind(10, 40), ik = selected_int_kind(5)
+
+    type :: vec
+        real(rk) :: x, y, z
+    end type vec
+
+    interface operator(+)
+        module procedure vadd
+    end interface
+
+    interface operator(-)
+        module procedure vminus
+    end interface
+
+    interface operator(*)
+        module procedure scalarprod
+    end interface
+
+contains
+    function vadd(a, b) result(c)
+        type(vec), intent(in) :: a, b
+        type(vec) :: c
+
+        c%x = a%x + b%x
+        c%y = a%y + b%y
+        c%z = a%z + b%z
+    end function vadd
+
+    function vminus(a, b) result(c)
+        type(vec), intent(in) :: a, b
+        type(vec) :: c
+
+        c%x = a%x - b%x
+        c%y = a%y - b%y
+        c%z = a%z - b%z
+    end function vminus
+
+    function scalarprod(a, b) result(c)
+        type(vec), intent(in) :: a, b
+        real(rk) :: c
+
+        c = a%x * b%x + a%y * b%y + a%z * b%z
+    end function scalarprod
+
+    function norm(a) result(b)
+        type(vec), intent(in) :: a
+        real(rk) :: b
+
+        b = sqrt(a * a)
+    end function norm
+end module vectors
