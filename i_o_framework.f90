22eb27a484528a77c18132bd19949f828bc4ab93
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-09 18:15:28 +0200

    added prettyprint subroutine for matrices

--- i_o_framework.f90
+++ i_o_framework.f90
@@ -1,3 +1,15 @@
 module io_manager
+    use vectors
+
+contains
+    subroutine prettyprint_real(mat, n)
+        real(rk), dimension(n, n), intent(in) :: mat
+        integer(ik), intent(in) :: n
+        integer(ik) :: i
+
+        do i = 1, n
+            write(6, *) mat(i, :)
+        end do
+    end subroutine prettyprint_real
 
 end module io_manager
