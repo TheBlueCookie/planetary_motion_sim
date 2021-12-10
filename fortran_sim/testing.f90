6a93a0a2c149294c26ac6b36e0532b95abb64881
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-10 14:03:13 +0200

    adjusted testing, starting with real parameters

--- fortran_sim/testing.f90
+++ fortran_sim/testing.f90
@@ -3,37 +3,26 @@ program tester
     use io_manager
     implicit none
 
-    integer(ik), parameter :: n = 2
+    integer(ik), parameter :: n = 2, m = 100
+    real(rk), parameter :: step = 1
 
-    type(vec), dimension(n) :: pos, res_forces
+    type(vec), dimension(n) :: pos, res_forces, vel
     type(vec), dimension(n, n) :: dist_vecs, force_vecs
     integer(ik) :: i
     real(rk), dimension(n, n) :: dists, mass_mat
     real(rk), dimension(n) :: mass
     type(vec) :: a, b
+    type(verlet_solution(n, m)) :: solution
 
-    a = vec(1, 2, 0)
-    b = vec(2, 1, 0)
+    mass(1) = 1.989e30 ! sun
+    mass(2) = 5.972e24 ! earth
 
-    pos(1) = vec(-1., 0., 0.)
-    pos(2) = vec(1., 0., 0.)
-!    pos(3) = vec(7, 8, 9)
-!    pos(4) = vec(10, 11, 12)
+    pos(1) = vec(0., 0., 0.)
+    pos(2) = vec(150e9, 0., 0.)
 
-    mass(1) = 2.
-    mass(2) = 5.
-!    mass(3) = 3
-!    mass(4) = 4
+    vel(1) = vec(0., 0., 0.)
+    vel(2) = vec(0., 30e3, 0.)
 
-    mass_mat = get_mass_matrix(mass, n)
-    dist_vecs = calc_distance_vecs(pos, n)
-    dists = calc_scalar_distances_inv(dist_vecs, n)
-    res_forces = calc_forces(dist_vecs, dists, mass_mat, n)
-
-    call prettyprint_real(dists, n)
-    call prettyprint_real(mass_mat, n)
-    call prettyprint_vec_mat(dist_vecs, n)
-    call prettyprint_vec_arr(res_forces, n)
-    print *, pot_energy(dists, mass_mat, n)
+    solution = vel_verlet(pos, vel, mass, step, m, n)
 
 end program tester
