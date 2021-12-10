e78c60ecb2cbcf254b91505bcda3f97cdd335078
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-10 14:21:01 +0200

    started running reasonable simulations

--- fortran_sim/testing.f90
+++ fortran_sim/testing.f90
@@ -3,26 +3,27 @@ program tester
     use io_manager
     implicit none
 
-    integer(ik), parameter :: n = 2, m = 100
-    real(rk), parameter :: step = 1
+    integer(ik), parameter :: n = 2, m = 100000
+    real(rk), parameter :: step = 100
 
-    type(vec), dimension(n) :: pos, res_forces, vel
+    type(vec), dimension(n) :: sim_pos, sim_forces, sim_vel
     type(vec), dimension(n, n) :: dist_vecs, force_vecs
     integer(ik) :: i
     real(rk), dimension(n, n) :: dists, mass_mat
     real(rk), dimension(n) :: mass
-    type(vec) :: a, b
-    type(verlet_solution(n, m)) :: solution
+
+    call initialize(n, m)
 
     mass(1) = 1.989e30 ! sun
     mass(2) = 5.972e24 ! earth
 
-    pos(1) = vec(0., 0., 0.)
-    pos(2) = vec(150e9, 0., 0.)
+    sim_pos(1) = vec(0., 0., 0.)
+    sim_pos(2) = vec(150e9, 0., 0.)
 
-    vel(1) = vec(0., 0., 0.)
-    vel(2) = vec(0., 30e3, 0.)
+    sim_vel(1) = vec(0., 0., 0.)
+    sim_vel(2) = vec(0., 30e3, 0.)
 
-    solution = vel_verlet(pos, vel, mass, step, m, n)
+    call vel_verlet(sim_pos, sim_vel, mass, step, m, n)
 
+    call prettyprint_vec_arr(pos(2, :), m)
 end program tester
