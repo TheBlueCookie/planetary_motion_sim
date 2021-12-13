4366967250775babdc0d421813a85bb92f0219f7
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-13 18:53:30 +0200

    added third body, still unstable

--- fortran_sim/testing.f90
+++ fortran_sim/testing.f90
@@ -3,8 +3,8 @@ program tester
     use io_manager
     implicit none
 
-    integer(ik), parameter :: n = 2, m = 100000
-    real(rk), parameter :: step = 100
+    integer(ik), parameter :: n = 3, m = 100000, k = 10, fstep = 100000
+    real(rk), parameter :: step = 300
 
     type(vec), dimension(n) :: sim_pos, sim_forces, sim_vel
     type(vec), dimension(n, n) :: dist_vecs, force_vecs
@@ -12,18 +12,23 @@ program tester
     real(rk), dimension(n, n) :: dists, mass_mat
     real(rk), dimension(n) :: mass
 
-    call initialize(n, m)
+    call initialize_sim(n, m, k)
 
     mass(1) = 1.989e30 ! sun
     mass(2) = 5.972e24 ! earth
+    mass(3) = 2.972e24
 
     sim_pos(1) = vec(0., 0., 0.)
     sim_pos(2) = vec(150e9, 0., 0.)
+    sim_pos(3) = vec(90e9, 20e9, 0.)
 
     sim_vel(1) = vec(0., 0., 0.)
-    sim_vel(2) = vec(0., 30e3, 0.)
+    sim_vel(2) = vec(0., 20e3, 0.)
+    sim_vel(3) = vec(5e3, 0., 0e3)
 
-    call vel_verlet(sim_pos, sim_vel, mass, step, m, n)
+    call vel_verlet(sim_pos, sim_vel, mass, step, n, m, k, fstep)
 
-    call prettyprint_vec_arr(pos(2, :), m)
+    ! call prettyprint_vec_arr(pos(2, :), m)
+
+    call export_all_sim_results(pos, vel, acc, forces, n, m, k)
 end program tester
