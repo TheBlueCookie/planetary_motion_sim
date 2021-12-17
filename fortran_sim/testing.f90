28be116975649d7f90395141291e1a7d5956258b
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-18 01:40:10 +0200

    removed all actual testing lines, file is obsolete and will be replaced

--- fortran_sim/testing.f90
+++ fortran_sim/testing.f90
@@ -3,32 +3,25 @@ program tester
     use io_manager
     implicit none
 
-    integer(ik), parameter :: n = 3, m = 100000, k = 10, fstep = 100000
-    real(rk), parameter :: step = 300
+    character(len = 120) :: parfile = 'sim_params.txt'
 
-    type(vec), dimension(n) :: sim_pos, sim_forces, sim_vel
-    type(vec), dimension(n, n) :: dist_vecs, force_vecs
-    integer(ik) :: i
-    real(rk), dimension(n, n) :: dists, mass_mat
-    real(rk), dimension(n) :: mass
+    call initialize_sim(parfile, n_body, n_step, f_step, s_step, step, d_dur)
+    !
+    !    mass(1) = 1.989e30 ! sun
+    !    mass(2) = 5.972e24 ! earth
+    !    mass(3) = 2.972e24
+    !
+    !    sim_pos(1) = vec(0., 0., 0.)
+    !    sim_pos(2) = vec(150e9, 0., 0.)
+    !    sim_pos(3) = vec(90e9, 20e9, 0.)
+    !
+    !    sim_vel(1) = vec(0., 0., 0.)
+    !    sim_vel(2) = vec(0., 20e3, 0.)
+    !    sim_vel(3) = vec(5e3, 0., 0e3)
 
-    call initialize_sim(n, m, k)
-
-    mass(1) = 1.989e30 ! sun
-    mass(2) = 5.972e24 ! earth
-    mass(3) = 2.972e24
-
-    sim_pos(1) = vec(0., 0., 0.)
-    sim_pos(2) = vec(150e9, 0., 0.)
-    sim_pos(3) = vec(90e9, 20e9, 0.)
-
-    sim_vel(1) = vec(0., 0., 0.)
-    sim_vel(2) = vec(0., 20e3, 0.)
-    sim_vel(3) = vec(5e3, 0., 0e3)
-
-    call vel_verlet(sim_pos, sim_vel, mass, step, n, m, k, fstep)
+    call vel_verlet(pos, vel, masses, step, n_body, n_step, s_step, f_step)
 
     ! call prettyprint_vec_arr(pos(2, :), m)
 
-    call export_all_sim_results(pos, vel, acc, forces, n, m, k)
+    ! call export_all_sim_results(pos, vel, acc, forces, n_body, n_step, s_step)
 end program tester
