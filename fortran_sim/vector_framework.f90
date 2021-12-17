3ece37b35da9b355df1b163394db7499c390bb9b
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-18 01:40:56 +0200

    added cross product and array to vector assignment overloading

--- fortran_sim/vector_framework.f90
+++ fortran_sim/vector_framework.f90
@@ -1,6 +1,9 @@
 module vectors
     implicit none
     integer, parameter :: rk = selected_real_kind(10, 40), ik = selected_int_kind(5)
+    real(rk), parameter :: pi = 4.0 * atan(1.0_rk)
+    !real(rk), parameter :: alpha_g = 192.85948 * pi / 180.0, delta_g = 27.12825 * pi / 180.0, l_ncp = 122.93192 * pi / 180.0
+    ! https://en.wikipedia.org/wiki/Astronomical_coordinate_systems J2000.0
 
     type :: vec
         real(rk) :: x, y, z
@@ -23,6 +26,7 @@ module vectors
     interface assignment(=)
         module procedure vec_to_vec
         module procedure scalar_to_vec
+        module procedure arr_to_vec
     end interface
 
 contains
@@ -78,6 +82,15 @@ contains
         b = sqrt(a * a)
     end function norm
 
+    function cross_product(a, b) result(c)
+        type(vec), intent(in) :: a, b
+        type(vec) :: c
+
+        c%x = a%y * b%z - a%z * b%y
+        c%y = a%z * b%x - a%x * b%z
+        c%z = a%x * b%y - a%y * b%x
+    end function cross_product
+
     subroutine vec_to_vec(a, b)
         type(vec), intent(inout) :: a
         type(vec), intent(in) :: b
@@ -95,4 +108,13 @@ contains
         a%y = b
         a%z = b
     end subroutine scalar_to_vec
+
+    subroutine arr_to_vec(a, b)
+        type(vec), intent(inout) :: a
+        real(rk), dimension(3), intent(in) :: b
+
+        a%x = b(1)
+        a%y = b(2)
+        a%z = b(3)
+    end subroutine arr_to_vec
 end module vectors
