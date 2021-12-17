c0e357cd4535f3c2cde64749d952a6fa3ded102e
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-18 01:42:07 +0200

    added generic plotting functions

--- python_visualization/visu_framework.py
+++ python_visualization/visu_framework.py
@@ -1,7 +1,10 @@
 import os
+
+import matplotlib.pyplot as plt
 import pandas as pd
 
-def read_sim_data(subdir_path):
+
+def read_sim_data(subdir_path: str):
     files = [f for f in os.listdir(subdir_path)]
     data = []
 
@@ -11,3 +14,11 @@ def read_sim_data(subdir_path):
                                 names=['posx', 'posy', 'posz', 'velx', 'vely', 'velz', 'accx', 'accy', 'accz', 'forx',
                                        'fory', 'forz']))
     return data
+
+
+def plot_3d_trajectories(planet_data: pd.DataFrame, ax):
+    ax.plot(planet_data.posx, planet_data.posy, planet_data.posz)
+
+
+def plot_2d_trajectories(planet_data: pd.DataFrame, ax):
+    ax.plot(planet_data.posx, planet_data.posy)
