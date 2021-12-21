0e0bc48e7b5c338dfb768a31153ce79811eaed67
Elias Ankerhold <elias.ankerhold@gmail.com>
2021-12-21 03:25:15 +0200

    added moon

--- src/fortran_sim/coordinate_conversion.f90
+++ src/fortran_sim/coordinate_conversion.f90
@@ -2,30 +2,32 @@ program sim_preperation
     use plan_sim
     implicit none
 
-    integer(ik), parameter :: body_count = 9 - 1
-    real(rk), dimension(body_count) :: periapsis, apoapsis, eccentricity, a, vel_at_periapsis
-    real(rk) :: gm
+    integer(ik), parameter :: body_count = 10 - 1
+    real(rk), dimension(body_count) :: periapsis, a, vel_at_periapsis
+    real(rk) :: gm, gme
     integer(ik) :: i
 
     ! https://nssdc.gsfc.nasa.gov/planetary/factsheet/
-    periapsis = [46.0, 107.5, 147.1, 206.6, 740.5, 1352.6, 2741.3, 4444.5]
+    periapsis = [46.0, 107.5, 147.1, 206.6, 740.5, 1352.6, 2741.3, 4444.5, 0.363]
 
     ! semi major axis
-    ! https://www.princeton.edu/~willman/planetary_systems/Sol/
-    a = [0.3870993, 0.723336, 1.000003, 1.52371, 5.2029, 9.537, 19.189, 30.0699]
+    ! https://www.princeton.edu/~willman/planetary_systems/Sol/ (planets) and https://en.wikipedia.org/wiki/Orbit_of_the_Moon (moon)
+    a = [0.3870993, 0.723336, 1.000003, 1.52371, 5.2029, 9.537, 19.189, 30.0699, 0.0025718815]
 
     periapsis = periapsis * 1e9
-    apoapsis = apoapsis * 1e9
     a = a * 1.495978707e11
 
     ! general graviational constant of the sun
     gm = 1.327124400e20 ! https://ssd.jpl.nasa.gov/astro_par.html
+    ! general graviational constant of the earth
+    gme = 398600.435507e9
 
     ! periapsis velocity
-    vel_at_periapsis = sqrt(gm * (2.0 / periapsis - 1 / a))
+    vel_at_periapsis(:8) = sqrt(gm * (2.0 / periapsis(:8) - 1 / a(:8)))
+    vel_at_periapsis(9) = sqrt(gme * (2.0 / periapsis(9) - 1 / a(9)))
 
     print *, a
-    print *, gm
+    print *, gm, gme
     print *, periapsis
     print *, vel_at_periapsis
 
